1b19a2ac235a3260c9e13c3bc39ee4164b431872
NATHALIA ROCHA MUNIZ <nathalia.3964@aluno.pr.senac.br>
2024-03-05 17:02:55 -0300

    começando o JavaScript

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+//boolean
+//true -> verdadeiro 
+//false -> falso
+
+const primeiroNumero = 10;
+const segundoNumero = 10;
+const cadastroAtivo = true;
+
+//console.log(primeiroNumero === segundoNumero); 
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2);
